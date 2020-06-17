732d92f37d4e58e942321bd5ee6ed520d081682c
Samy0412 <samantha.gadet@gmail.com>
2020-06-17 00:07:29 +0000

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+//Ensure de DOM had loaded
+$(document).ready(function () {
+  // --- our code goes here ---
+  // document.addEventListener("dblclick", (event) => {
+  //   console.log(event);
+  // });
+  // document.getElementById("tweet-text").addEventListener("keydown", (event) => {
+  //   console.log(event);
+  // });
+
+  $("#tweet-text").keyup(function () {
+    const value = $(this).val().length;
+    const counter = $("output");
+    counter.val(140 - value);
+    // /if (counter.val() <= 0) {
+    //   counter.addClass("red");
+    // } else {
+    //   counter.removeClass("red");
+    // }
+
+    counter.val() <= 0
+      ? counter.css("color", "red")
+      : counter.css("color", "#545149");
+  });
+});
